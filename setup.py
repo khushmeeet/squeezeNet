fa529d3cd70fac8b7d03dcac2aef5a11293706fc
Khushmeet <khushmeetsingh1996@gmail.com>
2017-01-23 22:25:06 +0530

    new version 0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'squeezenet',
   packages = ['squeezenet'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'A convolutional neural network with SqueezeNet based architecture',
   author = 'Khushmeet Singh',
   author_email = 'khushmeetsingh199@gmail.com',
   url = 'https://github.com/Khushmeet/squeezeNet', # use the URL to the github repo
-  download_url = 'https://github.com/Khushmeet/squeezeNet/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/Khushmeet/squeezeNet/tarball/0.2', # I'll explain this in a second
   keywords = ['Deep learning', 'Keras', 'Convolutional', 'Neural network', 'SqueezeNet'], 
   classifiers = [],
 )
